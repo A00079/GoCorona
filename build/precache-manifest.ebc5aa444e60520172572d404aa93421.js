d6ac540dd4df0033d69db86e214ebf4f16166720
A0079 <bajay00007@gmail.com>
2020-04-04 15:41:31 -0700

    Final try for this session Build

---
+++ build/precache-manifest.ebc5aa444e60520172572d404aa93421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b50c15238833efef17599d5e633685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29c98d7a4a1547e5d03d",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2f97d8c898576f329f14",
+    "url": "/static/css/main.4e80b0dd.chunk.css"
+  },
+  {
+    "revision": "29c98d7a4a1547e5d03d",
+    "url": "/static/js/2.d3db93f0.chunk.js"
+  },
+  {
+    "revision": "118cf714f6028dbe0232f6b83759d0ca",
+    "url": "/static/js/2.d3db93f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f97d8c898576f329f14",
+    "url": "/static/js/main.6216b799.chunk.js"
+  },
+  {
+    "revision": "6796558bb4b9de14c024",
+    "url": "/static/js/runtime-main.65d9faaa.js"
+  }
+]);
